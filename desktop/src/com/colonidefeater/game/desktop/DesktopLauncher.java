787eb9e74d6d5bf136085f4df8eee2bc82cf8e07
Anas KHABALI <mature.a@hotmail.fr>
2015-06-04 20:21:18 +0200

    Initialisation du projet

---
+++ desktop/src/com/colonidefeater/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.colonidefeater.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.colonidefeater.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
